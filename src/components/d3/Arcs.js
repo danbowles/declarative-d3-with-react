8f4c94ec263e591b3fcf6feacebb511fbfd0a00b
Dan Bowles <dbowles@alleyinterative.com>
2018-11-07 12:15:26 -0500

    linting + color palette

--- src/components/d3/Arcs.js
+++ src/components/d3/Arcs.js
@@ -1,8 +1,34 @@
 import * as d3 from 'd3';
+// import { interpolatePath } from 'd3-interpolate-path';
+
+function colors(index) {
+  const colorArray = [
+    '#3366cc',
+    '#dc3912',
+    '#ff9900',
+    '#109618',
+    '#990099',
+    '#0099c6',
+    '#dd4477',
+    '#66aa00',
+    '#b82e2e',
+    '#316395',
+    '#994499',
+    '#22aa99',
+    '#aaaa11',
+    '#6633cc',
+    '#e67300',
+    '#8b0707',
+    '#651067',
+    '#329262',
+    '#5574a6',
+    '#3b3eac',
+  ];
+  return colorArray[index % colorArray.length];
+}
 
 export default function D3Arcs() {
   const { plotData, arc } = this.props;
-  const color = d3.scaleOrdinal(d3.schemePastel1);
   const pie = d3.pie()
     .value(({ value }) => value)
     .sort(null);
@@ -15,26 +41,37 @@ export default function D3Arcs() {
 
   enter
     .append('path')
-    .attr('fill', (dataItem, index) => color(index))
-    .attr('d', arc);
+    .attr('fill', (dataItem, index) => colors(index))
+    .attr('d', arc)
+    .each((dataItem) => { this.currentPath = dataItem; });
 
   current.exit()
+    .attr('opacity', 1)
     .transition()
-    .attrTween('d', function attrTween(dataItem) {
-      const previous = d3.select(this).attr('d');
-      const newLine = arc(dataItem);
-      return interpolatePath(previous, newLine);
-    })
+    .attr('opacity', 0)
+    // .attrTween('d', arcTween)
+    // .attrTween('d', function attrTween(dataItem) {
+    //   const previous = d3.select(this).attr('d');
+    //   const newLine = arc(dataItem);
+    //   return interpolatePath(previous, newLine);
+    // })
     .remove();
 
   current
     .merge(enter)
     .select('path')
     .transition()
-    .attr('fill', (dataItem, index) => color(index))
-    .attrTween('d', function attrTween(dataItem) {
-      const previous = d3.select(this).attr('d');
-      const newLine = arc(dataItem);
-      return interpolatePath(previous, newLine);
-    });
+    .attr('fill', (dataItem, index) => colors(index))
+    .attrTween('d', arcTween);
+  // .attrTween('d', function attrTween(dataItem) {
+  //   const previous = d3.select(this).attr('d');
+  //   const newLine = arc(dataItem);
+  //   return interpolatePath(previous, newLine);
+  // });
+
+  function arcTween(newPath) {
+    const interpolated = d3.interpolate(this.currentPath, newPath);
+    this.currentPath = interpolated(0);
+    return (arg) => arc(interpolated(arg));
+  }
 }
--- src/services/chartData.js
+++ src/services/chartData.js
@@ -2,12 +2,12 @@ import * as d3 from 'd3';
 
 const randomInt = (n) => Math.round(Math.random() * n);
 
-export const pieData = () => d3.range(2000, 2006)
-  .map((dataItem, index) => ({
+export const pieData = () => d3.range(2000, 2011)
+  .map((dataItem, index) => (0.5 <= Math.random() ? {
     id: index,
     year: dataItem,
     value: randomInt(600),
-  }));
+  } : {}));
 
 export const lineData = () => d3.range(2000, 2005 + randomInt(10))
   .map((dataItem, index) => ({
